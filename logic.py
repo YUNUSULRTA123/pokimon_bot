045faf056aeb63befafeafa8462db8b7e8d3bb54
Yunus Azimli <afsananamazova01@gmail.com>
2025-08-17 13:47:31 +0400

    Закоммитено

--- logic.py
+++ logic.py
@@ -11,6 +11,8 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.weight = self.get_weight()
+        self.height = self.get_height()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -22,8 +24,8 @@ class Pokemon:
             pic = response.json()
             return (pic['sprites']['other']['official-artwork']['front_default'])
         else:
-            return "Ха ха оставайся без картинки ты плохой человек айай ☠️☠️"
-
+            return "Не удалось найти картинку :("
+        
     # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -32,9 +34,17 @@ class Pokemon:
             data = response.json()
             return (data['forms'][0]['name'])
         else:
-            return "Pikachu"
+            return "Не удалось узнать имя твоего глупого покемона:("
 
-
+    def get_weight(self):
+        url = f"https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            weight = response.json()
+            return "Высота твоего покемона - "(weight[weight])
+        else:
+            return "У твоего покемона 0 кг... странно как-то получается"
+        
     # Метод класса для получения информации
     def info(self):
         return f"Имя твоего покеомона: {self.name}"
@@ -42,6 +52,3 @@ class Pokemon:
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
-
-
-
