32c48d81329c4e41e85742084149f968ac2156b5
YUNUSULRTA123 <afsananamazova01@gmail.com>
2025-08-23 20:22:04 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import random
 sys.path.append(os.getcwd())
 from config import token
-from logic import Pokemon
+from logic import Pokemon, Wizard, Fighter
 
 bot = telebot.TeleBot(token) 
 
@@ -14,17 +14,22 @@ def start(message):
 /go - создать покемона
 /feed - покормить покемона
 /info - информация о покемоне
-    """)
+""")
 
 @bot.message_handler(commands=['go'])
-def go(message):
-    username = message.from_user.username
-    if username not in Pokemon.pokemons:
-        pokemon = Pokemon(username)
+def start(message):
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        chance = random.randint(1,3)
+        if chance == 1:
+            pokemon = Pokemon(message.from_user.username)
+        elif chance == 2:
+            pokemon = Wizard(message.from_user.username)
+        elif chance == 3:
+            pokemon = Fighter(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
-        bot.reply_to(message, "Ты уже создал себе покемона!")
+        bot.reply_to(message, "Ты уже создал себе покемона")
 
 @bot.message_handler(commands=["feed"])
 def feed_pokemon(message):
@@ -49,6 +54,20 @@ def info(message):
     else:
         bot.reply_to(message, "У тебя пока нет покемона. Используй /go")
 
+
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+            
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
 if __name__ == "__main__":
     print("Работает.....")
     bot.polling()
